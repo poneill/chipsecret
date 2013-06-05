6a8242845d67364fc4d32649a42447ee94c574ea
Patrick <synapse.and.syntax@gmail.com>
2013-06-05 17:14:01 -0400

    dealt with strandedness of reads

--- src/prelim_analysis.py
+++ src/prelim_analysis.py
@@ -6,8 +6,49 @@ from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from itertools import product,combinations
 
-AraCa = "Sample_AMS-ara-gal-Ec_AraCa/AraCa.map"
-AraCb = "Sample_AMS-ara-gal-Ec_AraCb/AraCb.map"        
+AraCa = "results/AraC/maps/AraCa.map"
+AraCb = "results/AraC/maps/AraCb.map"
+
+
+def interpret_map_file(filename,double_stranded=False):
+    chip_seq_data = [0 for _ in get_ecoli_genome()]
+    if double_stranded:
+        bk_chip_seq_data = [0 for _ in get_ecoli_genome()]
+    lines_so_far = 0
+    reads = 0
+    read_length = 51
+    n = len(get_ecoli_genome())
+    with open(filename) as f:
+        for line in f:
+            lines_so_far += 1
+            if line.startswith("UB-NGS"):
+                reads += 1
+                fields = line.split("\t")
+                strand = fields[1]
+                assert strand == "+" or strand == "-"
+                strand_direction = 1 if strand == "+" else -1
+                index = int(fields[3])
+                if reads % 10000 == 0:
+                    print index,lines_so_far/13460000.0
+                for i in xrange(read_length):
+                    if double_stranded:
+                        if strand_direction == 1:
+                            chip_seq_data[(index + i*strand_direction)%n] += 1
+                        else:
+                            bk_chip_seq_data[(index + i*strand_direction)%n] += 1
+                    else:
+                        chip_seq_data[(index + i*strand_direction)%n] += 1
+    if double_stranded:
+        return chip_seq_data,bk_chip_seq_data
+    else:
+        return chip_seq_data
+
+def write_chip_seq_data(chip_seq_data,filename):
+    genome = get_ecoli_genome()
+    with open(filename,'w') as f:
+        for (i,(base,val)) in verbose_gen(enumerate(zip(genome,chip_seq_data)),10**5):
+            f.write("%s,%s,%s\n"%(i,base,val))
+        
     
 # http://arep.med.harvard.edu/ecoli_matrices/dat/araC.dat
 arac_motif = ["TATGCCATAGCATTTTTATCCATAAGATTAGCGGATCCTACCTGACGC",
